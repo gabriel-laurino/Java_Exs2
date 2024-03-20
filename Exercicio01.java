557bcb8fe3e4cd5d7d2c5b68118afd1765e7cbcc
Gabriel-Laurino <gabriel_laurino@outlook.com>
2024-03-20 15:20:20 -0300

    Exercício1

---
+++ Exercicio01.java
@@ -0,0 +1,24 @@
+public class Exercicio01 {
+    public static void executar() {
+        double[] numeros = new double[5];
+        double soma = 0;
+        
+        for (int i = 0; i < numeros.length; i++) {
+            numeros[i] = Prompt.lerDecimal("Digite o número " + (i + 1) + ":");
+            soma += numeros[i];
+        }
+        
+        double media = soma / numeros.length;
+        Prompt.imprimir("Média: " + media);
+        
+        for (double num : numeros) {
+            if (num < media) {
+                Prompt.imprimir(num + " é menor que a média.");
+            } else if (num > media) {
+                Prompt.imprimir(num + " é maior que a média.");
+            } else {
+                Prompt.imprimir(num + " é igual à média.");
+            }
+        }
+    }
+}
